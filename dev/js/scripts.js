ed62081d78a70c59797726cce5aefa933e067814
lag369 <lag369@drexel.edu>
2023-06-03 16:02:36 -0400

    fixes

---
+++ dev/js/scripts.js
@@ -0,0 +1,10 @@
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools);
+
+
+
+GSDevTools.create();
