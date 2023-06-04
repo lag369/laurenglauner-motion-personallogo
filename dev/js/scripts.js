316da9befb17aea93d39dcd0b2014306de230858
lag369 <lag369@drexel.edu>
2023-06-04 16:52:59 -0400

    attempts

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,10 +1,22 @@
 import { gsap } from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-import { GSDevTools } from "gsap/GSDevTools";
+//import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+//import { GSDevTools } from "gsap/GSDevTools";
 
-gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools);
+gsap.registerPlugin(MotionPathPlugin);
 
 
 
-GSDevTools.create();
+function blockAni(){
+
+    let tl = gsap.timeline();
+
+    tl.from("#bottom-1", {duration:2, y:"-=500", ease:"bounce"})
+
+
+}
+
+let mainTl = gsap.timeline();
+mainTl.add(blockAni())
+
+;
