4a3baca4e843b0185e26922e8a90a7d7bdf3f325
lag369 <lag369@drexel.edu>
2023-06-06 23:10:42 -0400

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,18 +5,40 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(MotionPathPlugin);
 
-
+//INIT
+gsap.set("#bottom-1", {autoAlpha:0});
 
 function blockAni(){
 
     let tl = gsap.timeline();
 
-    tl.from("#bottom-1", {duration:2, y:"-=500", ease:"bounce"})
+    tl.to("#bottom-1", {duration:2, ease:"bounce"})
+    .to("#left-1", {duration:2, y:"-=500", ease:"bounce"})
+    .to("#right-1", {duration:2, y:"-=500", ease:"bounce"})
+
+    ;
+    return tl;
 
 
 }
 
+function lettersAni(){
+
+    let tl = gsap.timeline();
+
+    tl.to("#n", {opacity:0, duration:2, y:100, ease:"bounce"})
+    .to("#left-1", {duration:2, y:"-=500", ease:"bounce"})
+    .to("#right-1", {duration:2, y:"-=500", ease:"bounce"})
+
+    ;
+    return tl;
+
+
+}
+
+
 let mainTl = gsap.timeline();
 mainTl.add(blockAni())
+mainTl.add(lettersAni())
 
 ;
