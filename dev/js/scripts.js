56e1c5439b6a8e0f619cbc2606e92cabeacd87c7
lag369 <lag369@drexel.edu>
2023-06-15 17:33:27 -0400

    fixes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,29 +8,29 @@ gsap.registerPlugin(MotionPathPlugin);
 //INIT
 gsap.set("#text-path", {autoAlpha:0});
 
-function textAni(){
+function logoAni(){
 
     let tl = gsap.timeline();
 
-    tl.from("#logo-text", {duration:2, motionPath:{path:"#text-path",   align:"#text-path"}, ease:"bounce"})
+    tl.to("#bottom-1", {duration:2, transformOrigin: "50% 50%", motionPath:{path:"#b1-path", align:"#b1-path"}, ease:"bounce"})
+    .to("#left-1", {duration:2, transformOrigin: "50% 50%", motionPath:{path:"#l1-path", align:"#l1-path"}, ease:"bounce"})
+    .to("#right-1", {duration:2, transformOrigin: "50% 50%", motionPath:{path:"#r1-path", align:"#r1-path"}, ease:"bounce"})
+    .to("#bottom-2", {duration:2, transformOrigin: "50% 50%", motionPath:{path:"#b2-path", align:"#b2-path"}, ease:"bounce"})
+    .to("#left-2", {duration:2, transformOrigin: "50% 50%", motionPath:{path:"#l2-path", align:"#l2-path"}, ease:"bounce"})
+    .to("#right-2", {duration:2, transformOrigin: "50% 50%", motionPath:{path:"#r2-path", align:"#r2-path"}, ease:"bounce"})
+    .to("#top-2", {duration:2, transformOrigin: "50% 50%", motionPath:{path:"#t2-path", align:"#t2-path"}, ease:"bounce"})
+    .to("#top-1", {duration:2, transformOrigin: "50% 50%", motionPath:{path:"#t1-path", align:"#t1-path"}, ease:"bounce"})
 
     ;
     return tl;
 
 }
 
-function logoAni(){
+function textAni(){
 
     let tl = gsap.timeline();
 
-    tl.from("#bottom-1", {duration:2, motionPath:{path:"#b1-path", autoRotate: "true",  align:"#b1-path"}, ease:"bounce"})
-    .from("#left-1", {duration:2, motionPath:{path:"#l1-path", autoRotate: "true", align:"#l1-path"}, ease:"bounce"})
-    .from("#right-1", {duration:2, motionPath:{path:"#r1-path", autoRotate: "true", align:"#r1-path"}, ease:"bounce"})
-    .from("#bottom-2", {duration:2, motionPath:{path:"#b2-path", autoRotate: "true", align:"#b2-path"}, ease:"bounce"})
-    .from("#left-2", {duration:2, motionPath:{path:"#l2-path", autoRotate: "true", align:"#l2-path"}, ease:"bounce"})
-    .from("#right-2", {duration:2, motionPath:{path:"#r2-path", autoRotate: "true",  align:"#r2-path"}, ease:"bounce"})
-    .from("#top-2", {duration:2, motionPath:{path:"#t2-path", autoRotate: "true",  align:"#t2-path"}, ease:"bounce"})
-    .from("#top-1", {duration:2, motionPath:{path:"#t1-path", autoRotate: "true", align:"#t1-path"}, ease:"bounce"})
+    tl.to("#logo-text", {duration:2, motionPath:{path:"#text-path",   align:"#text-path"}, ease:"bounce"})
 
     ;
     return tl;
@@ -39,7 +39,9 @@ function logoAni(){
 
 
 let mainTl = gsap.timeline();
-mainTl.add(textAni())
+
 mainTl.add(logoAni())
+.add(textAni())
+
 
 ;
